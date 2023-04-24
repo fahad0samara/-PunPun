0619038aa837edaf4a1f5dd41f72ca3409f785fc
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-04-24 15:14:29 +0300

    updata the scrol

--- src/Helper/Scrolling.tsx
+++ src/Helper/Scrolling.tsx
@@ -1,36 +1,110 @@
-import React from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
-import "swiper/css";
-import "swiper/css/autoplay";
-import {Autoplay} from "swiper";
+// import {useState, useEffect, useRef} from "react";
+
+// import bigcommerce from "../assets/bigcommerce.svg";
+// import commerzbank from "../assets/commerzbank.svg";
+// import precommit from "../assets/precommit.svg";
+// import woocommerce from "../assets/woocommerce.svg";
+// import precommit0 from "../assets/precommit.svg";
+// import woocommerce0 from "../assets/woocommerce.svg";
+
+// const images = [
+//   bigcommerce,
+//   woocommerce0,
+//   precommit0,
+//   commerzbank,
+//   precommit,
+//   woocommerce,
+// ];
+
+// export default function Scrolling() {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const intervalRef = useRef(null);
+
+//   useEffect(() => {
+//     intervalRef.current = setInterval(() => {
+//       setActiveIndex((activeIndex + 1) % images.length);
+//     }, 2000); // adjust this value to change the interval between image transitions
+
+//     return () => clearInterval(intervalRef.current);
+//   }, [activeIndex, images.length]);
+
+//   return (
+//     <div className="flex h-16 w-full">
+//       {images.map((imageUrl, index) => (
+//         <div
+//           key={index}
+//           className={`w-full h-full transition-all duration-500 ease-in-out ${
+//             activeIndex === index ? "opacity-100" : "opacity-0"
+//           }`}
+//         >
+//           <img
+//             src={imageUrl}
+//             alt={`Image ${index + 1}`}
+//             className="h-9 w-9 object-contain mx-2"
+//           />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+import {useState, useEffect, useRef} from "react";
+import bigcommerce from "../assets/bigcommerce.svg";
+import commerzbank from "../assets/commerzbank.svg";
+import precommit from "../assets/precommit.svg";
+import woocommerce from "../assets/woocommerce.svg";
 
 const images = [
-  "https://via.placeholder.com/200x100?text=Image%201",
-  "https://via.placeholder.com/200x100?text=Image%202",
-  "https://via.placeholder.com/200x100?text=Image%203",
-  "https://via.placeholder.com/200x100?text=Image%204",
-  "https://images.unsplash.com/photo-1515343480029-43cdfe6b6aae?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDI2fHxsYXB0b3B8ZW58MHx8fHwxNjgxOTY1ODA4&ixlib=rb-4.0.3&h=1500",
+  bigcommerce,
+  commerzbank,
+  precommit,
+  woocommerce,
+  bigcommerce,
+  commerzbank,
+  precommit,
+  woocommerce,
+  bigcommerce,
+  commerzbank,
+  precommit,
+  woocommerce,
+  bigcommerce,
+  commerzbank,
+  precommit,
+  woocommerce,
 ];
 
 export default function Scrolling() {
+  const [scrollX, setScrollX] = useState(0);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      setScrollX(scrollX => scrollX - 60);
+    }, 2000); // adjust this value to change the interval between image transitions
+
+    return () => clearInterval(intervalRef);
+  }, []);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const containerWidth = containerRef.current.getBoundingClientRect().width;
+      if (scrollX < -containerWidth) {
+        setScrollX(0);
+      }
+    }
+  }, [scrollX]);
+
   return (
-    <Swiper
-      modules={[Autoplay]}
-      autoplay={{
-        delay: 2000, // adjust this value to change the interval between image transitions
-      }}
-      slidesPerView="auto"
-      spaceBetween={2}
-    >
+    <div className="w-full h-12 overflow-hidden relative" ref={containerRef}>
       {images.map((imageUrl, index) => (
-        <SwiperSlide key={index}>
-          <img
-            alt={`Image ${index + 1}`}
-            src={imageUrl}
-            className="card-image w-16"
-          />
-        </SwiperSlide>
+        <img
+          key={index}
+          src={imageUrl}
+          alt={`Image ${index + 1}`}
+          className="h-9 w-9 object-contain absolute top-0 left-0 transition-all duration-2000 ease-out"
+          style={{transform: `translateX(${scrollX + index * 60}px)`}}
+        />
       ))}
-    </Swiper>
+    </div>
   );
 }
